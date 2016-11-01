78ef9f80c5f9fdb6de6b3cb005e6b2519b933d38
RichGraham <Richard.Graham@nottingham.ac.uk>
2016-11-01 11:14:13 +0000

    Added N<lhcSize> folder to store the trained model information

--- PES_GP_Symm.f90
+++ PES_GP_Symm.f90
@@ -8,6 +8,17 @@ module PES_details
   end interface PES
 end module PES_details
 
+module RMSE_details 
+  interface RMSE_Calc
+     function RMSE_Calc( gridData, nGrid)
+       implicit none 
+       double precision:: RMSE_Calc
+       double precision, dimension(:,:) ::  gridData
+       integer :: nGrid
+     end function RMSE_Calc
+  end interface RMSE_Calc
+end module RMSE_details
+
 module GP_variables
   double precision, allocatable :: alpha (:), lScale(:), xTraining(:,:), xTrainingPerm(:,:)
   double precision expVar,NuggVar
@@ -19,6 +30,7 @@ end module GP_variables
 program GP
   use GP_variables
   use PES_details
+  use RMSE_details
   implicit none
   
   double precision, allocatable::  xStar(:)
@@ -32,33 +44,35 @@ program GP
   allocate (gridData(nDim+1,nGrid))
 
   !====Load hyperparameters====
-  write (filename, '( "N", I1, "/HyperParams_Symm.dat" )' )  nTraining
-  print *, filename
-  open (unit = 7, file = "../HyperParams_Symm.dat")
+  write (filename, '( "N", I4.4, "/HyperParams_Symm.dat" )' )  nTraining
+  open (unit = 7, file = filename)
   !Only need to read some as others are tied.
   read (7,*) lScale(4), lScale(3), lScale(2), lScale(1),expVar,NuggVar
   !Copy over the tied values
   lScale(5) = lScale(3)
   lScale(6) = lScale(4)
-  print *,"HyperParams",lScale(1), lScale(2), lScale(3), lScale(4), lScale(5), lScale(6),expVar,NuggVar
+  !print *,"HyperParams",lScale(1), lScale(2), lScale(3), lScale(4), lScale(5), lScale(6),expVar,NuggVar
   close(7)
   
   !====Load alpha coefficients====
-  open (unit = 7, file = "../alpha_Symm.dat")
+  write (filename, '( "N", I4.4, "/alpha_Symm.dat" )' )  nTraining
+  open (unit = 7, file = filename)
   do i=1,nTraining
      read (7,*) alpha(i)
-     print *,"alpha ",i, alpha(i)
+     !print *,"alpha ",i, alpha(i)
   end do
   close(7)
 
 
   !====Load training data x values ====
-  open (unit = 7, file = "../xTraining.dat")
+  write (filename, '( "N", I4.4, "/xTraining.dat" )' )  nTraining
+  open (unit = 7, file = filename)
+    
   do i=1,nTraining
-     read (7,*) xTraining(1,i), xTraining(2,i), xTraining(3,i), xTraining(4,i), xTraining(5,i), xTraining(6,i)
-     print *,"xTraining ", xTraining(1,i), xTraining(2,i), xTraining(3,i), xTraining(4,i), xTraining(5,i), xTraining(6,i)
+     read (7,*) xTraining(1,i), xTraining(2,i), xTraining(3,i), xTraining(4,i), xTraining(5,i), xTraining(6,i)  
   end do
   close(7)
+  
   !! Permute the training vectors
   xTrainingPerm = xTraining
   do i=1,nTraining
@@ -69,11 +83,10 @@ program GP
   end do
 
   !====Load grid data ====
-  open (unit = 7, file = "../rInv_HIt/co2CO_1074.lhc")
-  !open (unit = 7, file = "../co2CO_6744.lhc")
+  open (unit = 7, file = "GridData/co2CO_1074_rInv.lhc")
+  
   do i=1,nGrid
      read (7,*) gridData(1,i), gridData(2,i), gridData(3,i), gridData(4,i), gridData(5,i), gridData(6,i),gridData(7,i)
-     !print *,"gridData ", gridData(1,i), gridData(2,i), gridData(3,i), gridData(4,i), gridData(5,i), gridData(6,i), gridData(7,i)
   end do
   close(7)
 
@@ -83,23 +96,11 @@ program GP
   !xStar (1:6) = (/0.11845207,  0.11712603,  0.13018238,  0.13044285,  0.10768081,  0.10557117/)
     xStar (1:6) = (/0.47456526, 0.37298818, 0.49191578, 0.33957926, 0.3662692, 0.34482146 /)
 
-  print *,"xStar", xStar
+  !print *,"xStar", xStar
   dum=PES( xStar)
-  print *,"Prediction =",PES( xStar)
+  !print *,"Prediction =",PES( xStar)
 
-  !====Compute RMSE====
-  RMSE=0.0
-  nRMSE = 0
-  do i=1,nGrid
-     if( gridData(7,i) < 0.005) then
-        do j=1,nDim
-           xStar(j) = gridData(j,i)
-        end do
-        RMSE  =  RMSE + (gridData(7,i)-PES(xStar))**2
-        nRMSE = nRMSE +1
-     end if
-  end do
-  print *,Sqrt(RMSE/(1.0*nRMSE))
+  print*, RMSE_Calc( gridData, nGrid)
   
 end program GP
   
@@ -125,3 +126,39 @@ function PES(xStar)
   
   PES=kKern * expVar
 end function PES
+
+
+
+function RMSE_Calc( gridData, nGrid)
+  use GP_variables
+  use PES_details
+  implicit none
+
+  double precision, dimension(:,:) :: gridData
+  double precision, allocatable::  xStar(:)
+
+
+  double precision RMSE_calc
+
+  integer nGrid
+  integer i,nRMSE,j
+  double precision :: RMSE
+
+  allocate (xStar(nDim))
+  
+  !====Compute RMSE====
+  RMSE=0.0
+  nRMSE = 0
+  do i=1,nGrid
+     if( gridData(7,i) < 0.005) then
+        do j=1,nDim
+           xStar(j) = gridData(j,i)
+        end do
+        RMSE  =  RMSE + (gridData(7,i)-PES(xStar))**2
+        nRMSE = nRMSE +1
+     end if
+  end do
+  
+  RMSE_Calc = Sqrt(RMSE/(1.0*nRMSE))
+  
+end function RMSE_CALC
